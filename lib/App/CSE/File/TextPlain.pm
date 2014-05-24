116ba2bb4e47da97233fc779a32d28cc8afc9039
Jerome Eteve <jerome@eteve.net>
2014-05-24 11:46:35 +0100

    TextXTex inherits from TextPlain

--- lib/App/CSE/File/TextPlain.pm
+++ lib/App/CSE/File/TextPlain.pm
@@ -3,18 +3,16 @@ package App::CSE::File::TextPlain;
 use Moose;
 extends qw/App::CSE::File/;
 
-use App::CSE::File::ApplicationXRuby;
-use App::CSE::File::ApplicationXPerl;
-
 sub effective_object{
   my ($self) = @_;
   if( $self->file_path() =~ /\.pod$/ ){
     return $self->requalify('application/x-perl');
-  }
-  elsif( $self->file_path() =~ /\.rbw$/ ){
+  }  elsif( $self->file_path() =~ /\.ini$/ ){
+    return $self->requalify('application/x-wine-extension-ini');
+  }  elsif( $self->file_path() =~ /\.rbw$/ ){
     return $self->requalify('application/x-ruby');
-  }elsif( $self->file_path() =~ /\.tt$/ &&
-          $self->content() =~ /\[%.+%\]/ ){
+  } elsif( $self->file_path() =~ /\.tt$/ &&
+           $self->content() =~ /\[%.+%\]/ ){
     return $self->requalify('application/x-templatetoolkit');
   }
   return $self;
--- lib/App/CSE/File/TextXTex.pm
+++ lib/App/CSE/File/TextXTex.pm
@@ -1,15 +1,10 @@
 package App::CSE::File::TextXTex;
 
 use Moose;
-extends qw/App::CSE::File/;
-
-use App::CSE::File::ApplicationXWineExtensionIni;
+extends qw/App::CSE::File::TextPlain/;
 
 sub effective_object{
   my ($self) = @_;
-  if( $self->file_path() =~ /\.ini$/ ){
-    return App::CSE::File::ApplicationXWineExtensionIni->new({ cse => $self->cse(), mime_type => 'application/x-wine-extension-ini', file_path => $self->file_path() });
-  }
   return $self;
 }
 

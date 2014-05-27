3bcd5c54dad593881630709ae0b6a37953b4a1f9
Jerome Eteve <jerome@broadbean.com>
2014-05-27 16:56:06 +0100

    Do not reimplement effective_object in LaTeX

--- lib/App/CSE/File/TextXTex.pm
+++ lib/App/CSE/File/TextXTex.pm
@@ -3,9 +3,4 @@ package App::CSE::File::TextXTex;
 use Moose;
 extends qw/App::CSE::File::TextPlain/;
 
-sub effective_object{
-  my ($self) = @_;
-  return $self;
-}
-
 __PACKAGE__->meta->make_immutable();

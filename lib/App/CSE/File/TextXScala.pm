63a01a252e531c06d6c73334234259b93bfa7e24
Jerome Eteve <jerome.eteve@guardian.co.uk>
2018-04-23 15:18:10 +0100

    Added basic declarations extraction for scala files

--- lib/App/CSE/File/TextXScala.pm
+++ lib/App/CSE/File/TextXScala.pm
@@ -4,4 +4,10 @@ use Moose;
 
 extends qw/App::CSE::File/;
 
+sub _build_decl{
+    my ($self) = @_;
+    ( my @declarations ) = ( $self->content() =~ m/\s(?:class|trait|object|def|val|var)\s+(\w+)/gm );
+    return \@declarations;
+}
+
 __PACKAGE__->meta->make_immutable();
